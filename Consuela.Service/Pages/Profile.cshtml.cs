f43128a12353d4521fc584fc11b4e2500fb4bc83
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2022-05-27 03:05:20 -0400

    Mocked up a bad profile page.

---
+++ Consuela.Service/Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Consuela.Service.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
