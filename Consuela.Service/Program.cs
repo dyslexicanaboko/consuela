fb1b89bea0e779115a578cd96c324956348cf62f
Eli Hayon <dyslexicanaboko@gmail.com>
2022-07-31 22:29:58 -0400

    Found a bug I introduced yesterday by not testing the service directly. I forgot to wire up the excel class, doh. Getting rid of the other failed setup projects because I replaced them with Inno Setup which just works. Updated the version to 1.3.0.1 and the readme with the full info about what changed.

--- Consuela.Service/Program.cs
+++ Consuela.Service/Program.cs
@@ -1,4 +1,5 @@
 using Consuela.Service.Startup;
+using Microsoft.Extensions.Hosting.WindowsServices;
 using Serilog;
 
 namespace Consuela.Service
@@ -9,15 +10,25 @@ namespace Consuela.Service
         //https://github.com/serilog/serilog-extensions-hosting/blob/dev/samples/SimpleServiceSample/Program.cs
         public static int Main(string[] args)
         {
+            //If anything goes wrong it needs to be saved somewhere
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
+                .WriteTo.File(
+                    AppDomain.CurrentDomain.BaseDirectory + "\\Startup.log",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 3)
                 .CreateBootstrapLogger();
 
             try
             {
                 //WebApplication is the host
-                var builder = WebApplication.CreateBuilder(args);
+                //var builder = WebApplication.CreateBuilder(args);
+                var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+                {
+                    Args = args,
+                    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+                });
 
                 //Hang a Windows Service off of the WebApplication host
                 builder.Host.UseWindowsService();
--- ConsuelaWebSetup/UninstallSvc.bat
+++
@@ -1,4 +0,0 @@
-sc stop Consuela
-sc delete Consuela
-
-IF EXIST Profile.js DEL /F Profile.js
